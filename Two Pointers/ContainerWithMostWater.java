14ed5169bcd7d623a9d2a7ccca6cf818b9aa3e3e
problemsolver511 <problemsolver592@gmail.com>
2024-08-23 01:04:46 +0530

    added question description with testcase

--- Two Pointers/ContainerWithMostWater.java
+++ Two Pointers/ContainerWithMostWater.java
@@ -1,6 +1,21 @@
+/*
+
+You are given an integer array heights where heights[i] represents the height of the i'th bar.
+
+You may choose any two bars to form a container. Return the maximum amount of water a container can store.
+
+Input: height = [1,7,2,5,4,7,3,6]
+
+Output: 36
+
+Input: height = [2,2,2]
+
+Output: 4
+
+*/
+
 
 //  Method 1 - BruteForce Approach 
-
 
 class ContainerWithMostWater{
 	
